a3d7c51602f8297da314c48b472483b7d8b41b13
Karine Vitoria <karinevitoriasilvarodrigues@gmail.com>
2026-03-04 21:16:33 -0300

    Add files via upload

---
+++ DadosEntrada.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.poo.prjeqseggrau_poo1;
+
+/**
+ *
+ * @author Iftm
+ */
+public class DadosEntrada {
+    
+    
+    private double a; //pertence a classse
+    private double b;
+    private double c;
+    
+    
+    public double getA() {
+        
+        return a;
+    }
+    public void setA(double a) {
+        this.a = a;
+        //atributo a esta passando para a classe a
+    }
+    
+    public double getB() {
+        
+        return b;
+    }
+    public void setB(double b) {
+        this.b = b;
+        //atributo a esta passando para a classe a
+    }
+    
+    public double getC() {
+        
+        return c;
+    }
+    public void setC(double c) {
+        this.c = c;
+        //atributo a esta passando para a classe a
+    }
+    
+    
+    
+    
+    
+}
---
+++ PrjEqSegGrau_POO1.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.poo.prjeqseggrau_poo1;
+
+/**
+ *
+ * @author Iftm
+ */
+public class PrjEqSegGrau_POO1 {
+
+    public static void main(String[] args) {
+        DadosEntrada de = new DadosEntrada();
+        
+        de.setA(1);
+        de.setB(5);
+        de.setC(6);
+        
+        System.out.println("A: "+de.getA());
+        System.out.println("B: "+de.getB());
+        System.out.println("C: "+de.getC());
+    }
+}
